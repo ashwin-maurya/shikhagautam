b18fe15c78056e17bf76a53a9a31bacf2f78fa56
Ashwin Maurya <ashwinmaurya30@gmail.com>
2024-03-03 18:46:35 +0530

    Medical Certificate Changes

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,7 +9,7 @@ export default function AboutPage() {
         <title>About | Shikha Gautam</title>
         <meta
           name="description"
-          content="Shikha Gautam is created by Shikha Gautam. It is a collection of the best that’s happening in the world of travel & hospitality, including handpicked stays, unforgettable food experiences, mythology & folk tales, indigenous cultures - their practices and rituals, and about people you cannot forget easily."
+          content="Shikha Gautam is created by Shikha Gautam. It is a collection of the best that's happening in the world of travel & hospitality, including handpicked stays, unforgettable food experiences, mythology & folk tales, indigenous cultures - their practices and rituals, and about people you cannot forget easily."
         />
       </Helmet>
       <AboutInfo />
