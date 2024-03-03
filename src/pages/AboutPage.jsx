a06a9f4c30f1a0d9dd1c0a0477a688f761bc47a7
Ashwin Maurya <ashwinmaurya30@gmail.com>
2024-03-03 18:27:01 +0530

    first commit

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import AboutInfo from "../section/About/AboutInfo";
-import { Helmet, HelmetProvider } from "react-helmet";
+import { Helmet } from "react-helmet";
 
 export default function AboutPage() {
   return (
     <>
-      <HelmetProvider>
-        <Helmet>
-          <title>About | Shikha Gautam</title>
-          <meta
-            name="description"
-            content="Shikha Gautam is created by Shikha Gautam. It is a collection of the best that’s happening in the world of travel & hospitality, including handpicked stays, unforgettable food experiences, mythology & folk tales, indigenous cultures - their practices and rituals, and about people you cannot forget easily."
-          />
-        </Helmet>
-      </HelmetProvider>
-
+      <Helmet>
+        <title>About | Shikha Gautam</title>
+        <meta
+          name="description"
+          content="Shikha Gautam is created by Shikha Gautam. It is a collection of the best that’s happening in the world of travel & hospitality, including handpicked stays, unforgettable food experiences, mythology & folk tales, indigenous cultures - their practices and rituals, and about people you cannot forget easily."
+        />
+      </Helmet>
       <AboutInfo />
     </>
   );
